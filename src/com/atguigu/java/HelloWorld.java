e61dfb77bc026016bbd7a1f1ece9646f7698a1d4
tom_glb <good_glb@atguigu.com>
2019-06-12 13:27:42 +0800

    project01更改2

--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("324");
+        System.out.println("543543");
     }
 }
